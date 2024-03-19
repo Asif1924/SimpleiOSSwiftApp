152ea9d55e6b0b130d19a9fc5689910944410947
Asif1924 <asif1924dev@gmail.com>
2024-03-19 08:31:47 -0400

    Initial Commit

---
+++ SimpleiOSSwiftApp/SimpleiOSSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleiOSSwiftAppApp.swift
+//  SimpleiOSSwiftApp
+//
+//  Created by Asif Alli on 2024-03-19.
+//
+
+import SwiftUI
+
+@main
+struct SimpleiOSSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
