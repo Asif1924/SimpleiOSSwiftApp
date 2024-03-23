6192c50a404c4afca5a2f116482b31f79e668851
Asif1924 <asif1924dev@gmail.com>
2024-03-23 14:25:41 -0400

    Added button

--- SimpleiOSSwiftApp/ContentView.swift
+++ SimpleiOSSwiftApp/ContentView.swift
@@ -14,6 +14,19 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Hello, world2!")
+            Button(action: {
+                // Action for button tap
+                print("Button tapped!")
+            }) {
+                Text("My Button")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 200, height: 50)
+                    .background(Color.gray)
+                    .cornerRadius(8)
+                    .shadow(color: .black, radius: 4, x: 0, y: 2)
+            }
         }
         .padding()
     }
