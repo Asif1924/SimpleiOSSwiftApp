7fc553baa4758ebfc94b4f6a1a5ebf6c3f6f0fa2
Asif1924 <asif1924dev@gmail.com>
2024-03-23 17:42:42 -0400

    Added a form with 3 fields

--- SimpleiOSSwiftApp/ContentView.swift
+++ SimpleiOSSwiftApp/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value1 = ""
+    @State private var value2 = ""
+    @State private var value3 = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -26,6 +30,21 @@ struct ContentView: View {
                     .background(Color.gray)
                     .cornerRadius(8)
                     .shadow(color: .black, radius: 4, x: 0, y: 2)
+            }
+            Form {
+                Section(header: Text("Enter Data")) {
+                    // Text field for first value
+                    TextField("Value 1", text: $value1)
+                        .textContentType(.oneTimeCode)
+                    
+                    // Text field for second value
+                    TextField("Value 2", text: $value2)
+                        .textContentType(.oneTimeCode)
+                    
+                    // Text field for third value
+                    TextField("Value 3", text: $value3)
+                        .textContentType(.oneTimeCode)
+                }
             }
         }
         .padding()
